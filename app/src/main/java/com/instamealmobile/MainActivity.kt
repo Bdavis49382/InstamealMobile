0981a84e3d681b5c5ceec56dc772b398e58586aa
Ben Davis <bendavis493@gmail.com>
2024-05-21 13:28:48 -0600

    test change

--- app/src/main/java/com/instamealmobile/MainActivity.kt
+++ app/src/main/java/com/instamealmobile/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.instamealmobile.ui.theme.InstamealMobileTheme
 
@@ -33,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello World! Welcome to $name!",
         modifier = modifier
     )
 }
@@ -42,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     InstamealMobileTheme {
-        Greeting("Android")
+        Greeting("InstamealWeb")
     }
 }
