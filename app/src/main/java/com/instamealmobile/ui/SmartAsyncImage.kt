2e563866109f758a0009aa9640c540f812936008
Ben Davis <bendavis493@gmail.com>
2025-07-26 12:59:26 -0600

    https://trello.com/c/vtXYe2YB/168-feed-scrolling-glitch

--- app/src/main/java/com/instamealmobile/ui/SmartAsyncImage.kt
+++ app/src/main/java/com/instamealmobile/ui/SmartAsyncImage.kt
@@ -1,7 +1,10 @@
 package com.instamealmobile.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -24,19 +27,28 @@ fun SmartAsyncImage(url: String?, modifier: Modifier = Modifier, backupText: Str
         placeholder = painterResource(R.drawable.baseline_image_24),
         imageLoader = viewModel.imageLoader
         )
+    val fallbackRatio = 13f / 8f
     val aspectRatio = when (painter.state) {
         is AsyncImagePainter.State.Success -> {
             val size = painter.state.painter?.intrinsicSize
-            if (size != null && intrinsic && size.width > 0 && size.height > 0) size.width / size.height else 1f
+            if (size != null && intrinsic && size.width > 0 && size.height > 0) size.width / size.height else if (!intrinsic) 1f else fallbackRatio
         }
-        else -> 1f
+        else -> if (intrinsic) fallbackRatio else 1f
     }
-    Image(
-        painter = painter,
-        contentScale = ContentScale.Crop,
-        modifier = modifier
-            .aspectRatio(aspectRatio)
-            .clip(RoundedCornerShape(10.dp)),
-        contentDescription = null
-    )
+    Box(modifier = modifier
+        .fillMaxWidth()
+        .then(
+            aspectRatio.let { Modifier.aspectRatio(it)}
+        )
+    ) {
+        Image(
+            painter = painter,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxSize()
+                .clip(RoundedCornerShape(10.dp)),
+            contentDescription = null
+        )
+
+    }
 }
