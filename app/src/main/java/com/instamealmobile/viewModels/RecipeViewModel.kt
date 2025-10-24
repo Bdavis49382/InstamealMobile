f1a4dc3707e52f8c1006b6c09684316f6969ecd8
Ben Davis <bendavis493@gmail.com>
2025-10-24 14:03:42 -0600

    Added a better error message for when allrecipes doesn't have the right metadata

--- app/src/main/java/com/instamealmobile/viewModels/RecipeViewModel.kt
+++ app/src/main/java/com/instamealmobile/viewModels/RecipeViewModel.kt
@@ -9,6 +9,7 @@ import com.instamealmobile.network.MenuService
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
+import retrofit2.HttpException
 import javax.inject.Inject
 
 @HiltViewModel
@@ -26,10 +27,17 @@ class RecipeViewModel @Inject constructor(private val apiService: MenuService): 
                     is RecipeIdentifier.RecipeId -> apiService.getRecipe(recipe.id)
                     is RecipeIdentifier.RecipeLink -> apiService.getRecipeOnline(recipe.link)
                     is RecipeIdentifier.FullRecipe -> recipe.recipe
-                    else -> throw Exception("Cannot Get Recipe when one has not been selected.")
+                    else -> throw Exception("Cannot get a recipe when one has not been selected.")
                 }
                 _recipe.value = ApiState.Success(response)
-            } catch (e: Exception) {
+            } catch (e: HttpException) {
+                if (e.code() == 400) {
+                    _recipe.value = ApiState.Error("There is no recipe here, the website serving this recipe must have changed its mind. Try another recipe.")
+                } else {
+                    _recipe.value = ApiState.Error("Failed to fetch data: ${e.message}")
+                }
+            }
+            catch (e: Exception) {
                 _recipe.value = ApiState.Error("Failed to fetch data: ${e.message}")
             }
         }
