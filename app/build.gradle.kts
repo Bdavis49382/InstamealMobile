78e85ae186bb505ef103c9bf9848dae2085cde28
Ben Davis <bendavis493@gmail.com>
2025-10-29 15:21:56 -0600

    https://trello.com/c/8P1l6ukj/228-add-shopping-list-suggestions-while-typing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.instamealmobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "7"
+        versionCode = 8
+        versionName = "8"
 
         buildConfigField("String", "GOOGLE_CLIENT_ID", "\"$googleClientId\"")
         buildConfigField("String", "BACKEND_URL", "\"$backendURL\"")
--- app/src/main/java/com/instamealmobile/ui/pages/ShoppingListPage.kt
+++ app/src/main/java/com/instamealmobile/ui/pages/ShoppingListPage.kt
@@ -8,52 +8,38 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.instamealmobile.ui.CheckItem
 import com.instamealmobile.viewModels.ShoppingListViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.text.input.ImeAction
-import com.google.firebase.Firebase
-import com.google.firebase.auth.auth
 import com.instamealmobile.data.ApiState
 import com.instamealmobile.data.ShoppingItem
 import com.instamealmobile.data.SmallShoppingItem
+import com.instamealmobile.ui.ShoppingItemDropdown
 import com.instamealmobile.ui.placeholders.ShoppingListPlaceholder
-import kotlinx.coroutines.launch
 import sh.calvin.reorderable.ReorderableItem
 import sh.calvin.reorderable.rememberReorderableLazyListState
 
 @Composable
 fun ShoppingListPage(lazyListState: LazyListState) {
-    var newItemText by remember { mutableStateOf("") }
     val viewModel: ShoppingListViewModel =  viewModel()
-    val user = Firebase.auth
     val shoppingListState by viewModel.shoppingList.collectAsState()
-    val coroutineScope = rememberCoroutineScope()
     val reorderableLazyListState = rememberReorderableLazyListState(lazyListState) {  from, to ->
         viewModel.moveItem(from.index, to.index)
     }
 
     LaunchedEffect(Unit) {
         viewModel.fetchShoppingList()
+        viewModel.getSuggestions()
     }
 
     Column {
@@ -62,25 +48,7 @@ fun ShoppingListPage(lazyListState: LazyListState) {
                 .fillMaxWidth()
                 .padding(15.dp)
         ) {
-            TextField(
-                value = newItemText,
-                onValueChange = {newItemText = it},
-                label = { Text("Add Item") },
-                placeholder = { Text("New item for list...") },
-                keyboardOptions = KeyboardOptions(imeAction= ImeAction.Done),
-                keyboardActions = KeyboardActions(onDone = {
-                    val userInitial = user.currentUser?.displayName?.substring(0,1) ?: ""
-                    viewModel.addItemToList(newItemText, userInitial) {
-                        coroutineScope.launch {
-                            lazyListState.animateScrollToItem(0)
-                        }
-                    }
-                    newItemText = ""
-                }),
-                singleLine = true,
-                textStyle = TextStyle(fontSize = 20.sp),
-                modifier = Modifier.fillMaxWidth()
-            )
+            ShoppingItemDropdown(lazyListState)
         }
         Crossfade(targetState = shoppingListState, label = "ContentSwitch") { screenState ->
             when (screenState) {
